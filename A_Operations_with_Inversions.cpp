8bc8e68b3859779ff8017624adc8ff8b728e2dd4
Topurayhan554 <topurayhantipu@gmail.com>
2025-12-18 20:19:06 +0600

    codeforces problem A - Operations with Inversions is solved

---
+++ A_Operations_with_Inversions.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+        int mx = a[0];
+        int ops = 0;
+
+        for(int i=1;i<n;i++){
+            if(a[i] < mx){
+                ops++;
+            }
+            else{
+                mx = a[i];
+            }
+        }
+        cout << ops << endl;
+    }
+    return 0;
+}
