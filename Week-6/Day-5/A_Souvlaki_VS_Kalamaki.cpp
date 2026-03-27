978b2fc36566c9214286769e8ab8b5a33e92f2dc
Topurayhan554 <topurayhantipu@gmail.com>
2026-03-27 08:54:52 +0600

    week-6, day-6 completed

--- Week-6/Day-5/A_Souvlaki_VS_Kalamaki.cpp
+++ Week-6/Day-5/A_Souvlaki_VS_Kalamaki.cpp
@@ -1,24 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std; 
-
 void solve() {
     int n;
-    cin >> n;
-    map<int, int> counts;
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        counts[x]++;
+    if (!(cin >> n)) return;
+
+    unordered_map<int, int> freq;
+    for (int i = 0; i < n; ++i) {
+        int val;
+        cin >> val;
+        freq[val]++;
     }
 
     int total_pairs = 0;
-    for (auto const& [val, freq] : counts) {
-        total_pairs += freq / 2;
+    for (auto const& [val, count] : freq) {
+        total_pairs += count / 2;
     }
 
-    int kalamaki_rounds = (n - 1) / 2;
+    int even_rounds = (n - 1) / 2;
 
-    if (total_pairs >= kalamaki_rounds) {
+    if (total_pairs >= even_rounds) {
         cout << "YES" << endl;
     } else {
         cout << "NO" << endl;
@@ -28,8 +28,9 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int t;
-    cin >> t;
+    if (!(cin >> t)) return 0;
     while (t--) {
         solve();
     }
