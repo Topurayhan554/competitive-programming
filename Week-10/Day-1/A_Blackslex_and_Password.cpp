1da58d19e651e63f930e2d752af782d7f556c8db
Topurayhan554 <topurayhantipu@gmail.com>
2026-04-19 20:06:03 +0600

    week-10, day-1 completed

---
+++ Week-10/Day-1/A_Blackslex_and_Password.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+void solve() {
+    int k, x;
+    cin >> k >> x;
+    
+    cout << (k * x + 1) << "\n";
+}
+
+int main() {
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
