d7cdcde37b8b7123314e79cc3208a308c7330029
Topurayhan554 <topurayhantipu@gmail.com>
2026-05-11 13:56:07 +0600

    week-13, day-2 completed

---
+++ Week-13/Day-2/D_Remove_Two_Letters.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int ans = n - 1;
+
+    for (int i = 0; i < n - 2; ++i) {
+        if (s[i] == s[i + 2]) {
+            ans--;
+        }
+    }
+
+    cout << ans << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
