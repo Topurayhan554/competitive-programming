6a7e226fce6054d43d76d42268115813272e07f2
Topurayhan554 <topurayhantipu@gmail.com>
2026-06-28 11:26:16 +0600

    week-19, day-1 completed

---
+++ Week-19/Day-1/C_Isamatdin_and_His_Magic_Wand.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> a(n);
+
+        int has_even = false;
+        int has_odd = false;
+
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+            if(a[i] % 2 == 0){
+                has_even = true;
+            }else{
+                has_odd = true;
+            }
+        }
+
+        if(has_even && has_odd){
+            sort(a.begin(), a.end());
+        }
+
+        for(int i=0;i<n;i++){
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
