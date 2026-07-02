709234bb4458cc6e659c0b7c38eaa13a4d98ddeb
Biplob Roy <topurayhantipu@gmail.com>
2026-07-02 21:17:34 +0600

    week-19, day-5 completed

---
+++ Week-19/Day-5/A_Divide_And_Conqure.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int x, y;
+    cin >> x >> y;
+    if (x % y == 0) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
