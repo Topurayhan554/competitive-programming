3d3cc8ebcafaae716d9ca9789beaa4be5571ccde
Topurayhan554 <topurayhantipu@gmail.com>
2026-07-21 12:41:54 +0600

    week-22, day-3 completed

---
+++ Week-22/Day-3/B_Yet_Another_Constructive.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    long long n, k, m;
+    cin >> n >> k >> m;
+    
+    if (k > m) {
+        cout << "NO\n";
+    } else {
+        cout << "YES\n";
+        for (int i = 1; i <= n; ++i) {
+            if (i % k == 0) {
+                cout << m - k + 1 << (i == n ? "" : " ");
+            } else {
+                cout << 1 << (i == n ? "" : " ");
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
