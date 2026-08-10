3c2fc14d7783a8efaab2b40e1a98a295a09c30d1
Topu Rayhan <topurayhantipu@gmail.com>
2026-08-10 19:53:34 +0600

    week-25, day-2 completed

---
+++ Week-25/Day-2/A_The_Best_Card.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    
+    for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    if (isPrime(n + 1)) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    if (cin >> t) {
+        while (t--) {
+            solve();
+        }
+    }
+    return 0;
+}
