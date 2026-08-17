da0df8c7a1dbf3790dddcd9544c8aef77d476324
Topu Rayhan <topurayhantipu@gmail.com>
2026-08-17 10:50:20 +0600

    week-26, day-2 completed

---
+++ Week-26/Day-2/B_Lasers.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n, m;
+    long long x, y;
+    cin >> n >> m >> x >> y;
+
+    for(int i=0;i<n;i++){
+        long long a;
+        cin >> a;
+
+    }
+
+    for(int i=0;i<m;i++){
+        long long b;
+        cin >> b;
+
+    }
+
+    cout << n + m << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
